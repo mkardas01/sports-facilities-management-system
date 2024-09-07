28706884752f9c36925a4264c7bf4be74769484b
jakubklosin <110457260+jakubklosin@users.noreply.github.com>
2024-09-07 15:26:04 +0200

    Models for details response

---
+++ api/src/main/java/put/poznan/sport/response/SportFacilityDetailsResponse.java
@@ -0,0 +1,26 @@
+package put.poznan.sport.response;
+
+import lombok.Builder;
+import lombok.Data;
+import put.poznan.sport.dto.Coach.CoachCreateResponse;
+import put.poznan.sport.entity.SportEquipment;
+import put.poznan.sport.entity.openHour.OpenHour;
+
+import java.util.List;
+
+@Data
+@Builder
+public class SportFacilityDetailsResponse {
+    private Integer id;
+    private String name;
+    private String description;
+    private String address;
+    private String type;
+    private boolean membershipRequired;
+    private String imageUrl;
+    private OpenHour openHours;
+    private List<CoachCreateResponse> coaches;
+    private List<SportEquipment> equipment;
+    private Double averageRating;
+    private List<CoachAverageRating> coachRatings;
+}
