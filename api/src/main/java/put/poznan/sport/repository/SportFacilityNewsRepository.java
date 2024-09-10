54138d6fa84a1ef965ed6e01a0c5754a0498bc82
jakubklosin <110457260+jakubklosin@users.noreply.github.com>
2024-09-11 00:26:05 +0200

    Updated Details Controller returns News&Sessions

--- api/src/main/java/put/poznan/sport/repository/SportFacilityNewsRepository.java
+++ api/src/main/java/put/poznan/sport/repository/SportFacilityNewsRepository.java
@@ -3,5 +3,8 @@ package put.poznan.sport.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import put.poznan.sport.entity.SportFacilityNews;
 
+import java.util.List;
+
 public interface SportFacilityNewsRepository extends JpaRepository<SportFacilityNews, Integer> {
+    List<SportFacilityNews> findBySportFacilitiesId(Integer sportFacilitiesId);
 }
